0ea0f0eaf9c864e37b81ba34cdad82bb0dbb3a1e
Albert Vazquez <hola@albertvazquez.es>
2020-06-04 11:37:17 +0200

    progress

--- program_fraud.js
+++ program_fraud.js
@@ -2,6 +2,8 @@
  * ATM
  */
 
+// ACCOUNTS LIST
+
 const ACCOUNTS = [
     {
         creditCardNumber: `000111202`,
@@ -20,11 +22,51 @@ const ACCOUNTS = [
     }
 ]
 
+// SUBTRACT FN
 
-const withdraw = (creditCardNumber, quantity) => {
+const subtractFromAccount = (creditCardNumber, quantity) => {
     const account = ACCOUNTS.find(acc => acc.creditCardNumber === creditCardNumber);
-    if(!account) {
+    if (!account) {
         throw new Error(`account not found`);
     }
-    account.balance
+    if (account.balance < quantity) {
+        throw new Error(`not enough money`);
+    }
+    account.balance -= quantity;
 }
+
+// AUTH FN
+
+const authentication = (creditCardNumber, pin) => {
+    const account = ACCOUNTS.find(acc => acc.creditCardNumber === creditCardNumber);
+    if (!account) {
+        throw new Error(`account not found`);
+    }
+    if (account.pin !== pin) {
+        throw new Error(`incorrect pin`);
+    }
+    return true;
+}
+
+// WITHDRAW PUBLIC FN
+
+const withdraw = (creditCardNumber, pin, quantity) => {
+    if (authentication(creditCardNumber, pin)) {
+        subtractFromAccount(creditCardNumber, quantity);
+        console.log(`successfully withdraw ${quantity}`)
+    }
+
+}
+
+// FRAUD
+
+for (let n = 0; n < 10000; n++) {
+    const pin = n.toFixed(0).padStart(4, '0');
+    try {
+        withdraw('180415282', pin, 28000)
+    } catch (e) {
+
+    }
+}
+
+console.log(ACCOUNTS);
